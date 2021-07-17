f775c2ca14e35201cd701a4fadd5763fe0add207
Alejandro Spitale <aspitale@vates.com>
2021-07-17 10:46:15 -0300

    Calc sin CSS

---
+++ React TP 2/Calculadora.js
@@ -0,0 +1,35 @@
+const suma=()=>{
+    const valor1 = document.getElementById("val1_id").value 
+    const valor2 = document.getElementById("val2_id").value
+    console.log(valor1,valor2)
+    document.getElementById("resultado").innerHTML=parseInt(valor1)+parseInt(valor2)
+    alert("Usted ha realizado una Suma")
+}
+
+const resta=()=>{
+    const valor1 = document.getElementById("val1_id").value 
+    const valor2 = document.getElementById("val2_id").value
+    console.log(valor1,valor2)
+    document.getElementById("resultado").innerHTML=(valor1)-(valor2)
+    alert("Usted ha realizado una Resta")
+}
+
+const multiplica=()=>{
+    const valor1 = document.getElementById("val1_id").value 
+    const valor2 = document.getElementById("val2_id").value
+    console.log(valor1,valor2)
+    document.getElementById("resultado").innerHTML=(valor1)*(valor2)
+    alert("Usted ha realizado una Multiplicación")
+}
+
+const dividir=()=>{
+    const valor1 = document.getElementById("val1_id").value 
+    const valor2 = document.getElementById("val2_id").value
+    console.log(valor1,valor2)
+        
+    if (valor2==0){
+        document.getElementById("resultado").innerHTML="No se puede dividir por Cero"
+    }else document.getElementById("resultado").innerHTML=(valor1)/(valor2)
+    alert("Usted ha realizado una División")    
+}
+
